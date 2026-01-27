4dd383383accb2393984487aaf677cfa7457c803
adithya <adithyavarasiddi26@gmail.com>
2026-01-27 19:12:42 +0530

    Update backend index.js with new changes

--- grocery-react-app/backend/index.js
+++ grocery-react-app/backend/index.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 
 const pool = new Pool({
   user: 'postgres', // change to your postgres user
@@ -597,4 +599,4 @@ app.post('/reset-password', authenticate, async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Backend running on http://localhost:5000'));
+app.listen(PORT, () => console.log('Backend running on http://localhost:5000'));
